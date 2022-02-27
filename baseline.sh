a0030f8cc92a99a6db22f9aca595adb359013248
Tulika Mishra <pakhi10801@gmail.com>
2022-02-27 11:48:03 +0530

    added 10layer baseline and 6layerbaseline

--- baseline.sh
+++ baseline.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH --job-name=base
-#SBATCH --error=base.%J.err
-#SBATCH --output=base.%J.out
+#SBATCH --job-name=10layer
+#SBATCH --error=10layer.%J.err
+#SBATCH --output=10layer.%J.out
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 
@@ -12,7 +12,7 @@ module load cuda/10.0
 module load cuDNN/cuda_9.2_7.2.1
 
 source activate baseline 
-python main.py -m UncertainGCN -d cifar10 -c 10
+python main.py -m UncertainGCN -d cifar10 -c 10 -layers 10
 
 source deactivate
 
