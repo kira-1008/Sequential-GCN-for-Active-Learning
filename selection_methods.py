78aa0641ece6fc2c66c05147300041d5167b5fd8
Tulika Mishra <56957714+kira-1008@users.noreply.github.com>
2022-08-30 12:42:25 +0530

    updated selection.py to have age scoring function

--- selection_methods.py
+++ selection_methods.py
@@ -3,11 +3,16 @@ import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
 import torch.optim as optim
+import networkx as nx
+import os
+import scipy as sc
 # Custom
 from config import *
 from models.query_models import VAE, Discriminator, GCN
 from data.sampler import SubsetSequentialSampler
 from kcenterGreedy import kCenterGreedy
+from sklearn.cluster import KMeans
+from sklearn.metrics.pairwise import euclidean_distances
 
 def BCEAdjLoss(scores, lbl, nlbl, l_adj):
     lnl = torch.log(scores[lbl])
@@ -18,22 +23,24 @@ def BCEAdjLoss(scores, lbl, nlbl, l_adj):
     return bce_adj_loss
 
 
-def aff_to_adj(x, k=20,y=None):
+def aff_to_adj(x,k=20,y=None):
     x = x.detach().cpu().numpy()
     adj = np.matmul(x, x.transpose())
     adj +=  -1.0*np.eye(adj.shape[0])
     n = adj.shape[0]
-    # for i in range(0,n):
-    #     vals=[(adj[i][x],x) for x in range(0,n)]
-    #     vals.sort()
-    #     vals = vals[:k]
-    #     adj[i]=[adj[i][x] if (adj[i][x],x) in vals else 0 for x in range(0,n)]
+    graph_adj_list={}
+    for i in range(0,n):
+         vals=[(adj[i][x],x) for x in range(0,n)]
+         vals.sort()
+         vals = vals[-k:]
+         graph_adj_list[i]=[value[1] for value in vals] 
+         adj[i]=[adj[i][x] if (adj[i][x],x) in vals else 0 for x in range(0,n)]
     adj_diag = np.sum(adj, axis=1) #rowise sum
     adj = np.matmul(adj, np.diag(1/adj_diag))
     adj = adj + np.eye(adj.shape[0])
     adj = torch.Tensor(adj).cuda()
 
-    return adj
+    return adj,graph_adj_list
 
 def read_data(dataloader, labels=True):
     if labels:
@@ -202,31 +209,66 @@ def get_kcg(models, labeled_data_size, unlabeled_loader):
         other_idx = [x for x in range(SUBSET) if x not in batch]
     return  other_idx + batch
 
+#graph central
+def centralissimo(G):
+    centralities = []
+    #centralities.append(nx.degree_centrality(G))       #print 'degree centrality: check.'
+    #centralities.append(nx.closeness_centrality(G))    #print 'closeness centrality: check.'
+    #centralities.append(nx.betweenness_centrality(G))  #print 'betweenness centrality: check.'
+    #centralities.append(nx.eigenvector_centrality(G))  #print 'eigenvector centrality: check.'
+    centralities.append(nx.pagerank(G))                #print 'page rank: check.'
+    #centralities.append(nx.harmonic_centrality(G))
+    L = len(centralities[0])
+    Nc = len(centralities)
+    cenarray = np.zeros((Nc,L))
+    for i in range(Nc):
+    	cenarray[i][list(centralities[i].keys())]=list(centralities[i].values())
+    normcen = (cenarray.astype(float)-np.min(cenarray,axis=1)[:,None])/(np.max(cenarray,axis=1)-np.min(cenarray,axis=1))[:,None]
+    return normcen
 
-# Select the indices of the unlablled data according to the methods
-def query_samples(model, method, data_unlabeled, subset, labeled_set, cycle, args):
+#calculate the percentage of elements smaller than the k-th element
+def perc(input,k):
+  idx = (input[i]<input[k] for i in range(0,len(input)))
+  return np.sum(idx)/float(len(input))
+
+#calculate the percentage of elements larger than the k-th element
+def percd(input,k):
+  idx = (input[i]>input[k] for i in range(0,len(input)))
+  return np.sum(idx)/float(len(input))
+
+# Select the indices of the unlablled subset, labeled_set, cycle, args data according to the methods
+def query_samples(model, method, data_unlabeled,):
 
     if method == 'Random':
         arg = np.random.randint(SUBSET, size=SUBSET)
 
-    if (method == 'UncertainGCN') or (method == 'CoreGCN'):
+    if (method == 'UncertainGCN') or (method == 'CoreGCN') or (method == 'Age'):
         # Create unlabeled dataloader for the unlabeled subset
         unlabeled_loader = DataLoader(data_unlabeled, batch_size=BATCH, 
                 sampler=SubsetSequentialSampler(subset+labeled_set), # more convenient if we maintain the order of subset
                                     pin_memory=True)
         binary_labels = torch.cat((torch.zeros([SUBSET, 1]),(torch.ones([len(labeled_set),1]))),0)
 
-
         features = get_features(model, unlabeled_loader)
         features = nn.functional.normalize(features)
-        adj = aff_to_adj(features)
+        print("Getting adj matrix")
+        adj,adj_list = aff_to_adj(features)
+
+        #TODO change this to get a variable argument as dataset name
+        print("Started creation of networkx graph")
+        directory = "res/cifar10/graphcentrality/"
+        if not os.path.exists(directory):
+          os.makedirs(directory)
+        G = nx.Graph(adj_list)
+        normcen = centralissimo(G)	#the larger the score is, the more representative the node is in the graph
+        np.savetxt(directory+'normcen', normcen, fmt='%.6f', delimiter=' ', newline='\n')
+
+
         gcn_module = GCN(nfeat=features.shape[1],
                          nhid=args.hidden_units,
                          nclass=1,
-                         dropout=args.dropout_rate,nlayer=args.num_layers,
-                         norm_mode=args.norm_mode,
-                         norm_scale=args.norm_scale).cuda()
-
+                         dropout=args.dropout_rate,nlayer=args.num_layers).cuda()
+                                
         models      = {'gcn_module': gcn_module}
 
         optim_backbone = optim.Adam(models['gcn_module'].parameters(), lr=LR_GCN, weight_decay=WDECAY)
@@ -245,6 +287,14 @@ def query_samples(model, method, data_unlabeled, subset, labeled_set, cycle, arg
             loss.backward()
             optimizers['gcn_module'].step()
 
+        print("Loading normality scores")
+        normcen = np.loadtxt("res/cifar10"+"/graphcentrality/normcen")
+        print("Successfully loaded")
+        cenperc = np.asarray([perc(normcen,i) for i in range(len(normcen))])
+        basef=0.9
+        #time sensitive parameters
+        gamma = np.random.beta(1, 1.005-basef**cycle)
+        alpha = beta = (1-gamma)/2
 
         models['gcn_module'].eval()
         with torch.no_grad():
@@ -261,11 +311,29 @@ def query_samples(model, method, data_unlabeled, subset, labeled_set, cycle, arg
                 other_idx = [x for x in range(SUBSET) if x not in batch2]
                 arg = other_idx + batch2
 
-            else:
+            elif method == "UncertainGCN":
 
                 s_margin = args.s_margin 
                 scores_median = np.squeeze(torch.abs(scores[:SUBSET] - s_margin).detach().cpu().numpy())
                 arg = np.argsort(-(scores_median))
+
+            elif method == "Age":
+                print("Inside AGE")
+                scores_subset = scores[:SUBSET].detach().cpu().numpy()
+                prob = [scores_subset,1-scores_subset]
+                prob = np.transpose(prob)
+                entropy = sc.stats.entropy(prob)
+                print(entropy[:10])
+                #train_mask = sample_mask(idx_train, labels.shape[0])
+    	          #entropy[train_mask+val_mask+test_mask]=-100
+                entrperc = np.asarray([perc(entropy,i) for i in range(len(entropy))])
+                # since for us gcn is doing binary classification number of classes NCL = 2
+                kmeans = KMeans(n_clusters=2, random_state=0).fit(prob)
+                ed=euclidean_distances(prob,kmeans.cluster_centers_)
+                ed_score = np.min(ed,axis=1)	#the larger ed_score is, the far that node is away from cluster centers, the less representativeness the node is
+                edprec = np.asarray([percd(ed_score,i) for i in range(len(ed_score))])
+                finalweight = alpha*entrperc + beta*edprec + gamma*cenperc
+                arg=np.argsort(finalweight)
 
             print("Max confidence value: ",torch.max(scores.data))
             print("Mean confidence value: ",torch.mean(scores.data))
